ff911fd97010a85188762e6e16af00efb18e0d50
Rukmin Tharanga <rukmin.official@gmail.com>
2024-10-29 23:30:04 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="css/style.css">
+	  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	  <link rel="stylesheet" href="lib/owl.carousel.min.css">
+	  <link rel="stylesheet" href="lib/owl.theme.default.min.css">
+	  <link rel="icon" href="images/logo.png">
+	  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&family=Nunito:wght@400;700&display=swap" rel="stylesheet">
+	  <link rel="stylesheet" href="lib/animate.css">
+	<title>MY WEB</title>
+</head>
+<body>
+
+	<?php
+		include "header.html"; 
+		include "content_about_intro.html" ;
+		include "content_about_personal_info.html" ;
+		include "content_about_characteristics.html" ;
+		include "content_education.html" ;
+		include "content_extra_ca.html" ;
+		include "content_gallery.html" ;
+		include "content_contact.html" ;
+		include "footer.html" ;
+	?>
+	
+
+  <script src="lib/jquery-3.6.4.min.js"></script>
+  <script src="lib/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="lib/jquery.smooth-scroll.js"></script>
+  <script src="lib/imagesloaded.pkgd.min.js"></script>
+  <script src="lib/owl.carousel.min.js"></script>
+  <script src="lib/typed.js"></script>
+  <script src="lib/jquery.waypoints.min.js"></script>
+  <script src="lib/jquery.filterizr.min.js"></script>
+  <script src="js/app.js"></script>	
+</body>
+</html>
